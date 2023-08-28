6645f326550a7d12dfe40f7b49c4675994a9f79e
Layne28 <lfrechette1204@gmail.com>
2023-08-28 11:48:49 -0400

    Added README and modified setup.py

---
+++ README.md
@@ -0,0 +1,2 @@
+This package provides tools for analyzing particle-based simulation data.
+Currently, it only supports analysis of data provided in h5md format.
--- build/lib/AnalysisTools/__init__.py
+++ build/lib/AnalysisTools/__init__.py
@@ -2,4 +2,4 @@ from . import particle_io
 from . import measurement_tools
 from . import structure_factor
 
-__all__ = ["particle_io", "measurement_tools", "structure_factor"]
+__all__ = ["particle_io", "structure_factor"]
