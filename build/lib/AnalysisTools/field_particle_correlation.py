539ad97e90f90c76432e6cd6f09763858cdda07d
Layne28 <lfrechette1204@gmail.com>
2024-05-07 14:08:36 -0700

    added pressure correlation calculation

--- build/lib/AnalysisTools/field_particle_correlation.py
+++ build/lib/AnalysisTools/field_particle_correlation.py
@@ -15,25 +15,28 @@ def main():
     particle_file = sys.argv[1]
     noise_file = sys.argv[2]
     quantity = sys.argv[3] #density or pressure 
-
-    do_print_density=0
     
     particle_traj = io.load_traj(particle_file)
     noise_traj = io.load_noise_traj(noise_file)
     if quantity=='density':
-        corr = correlate_density(particle_traj, noise_traj, noise_file)
+        corr, corr_sparse, noise_mean, quantity_mean, quantity_mean_sparse = correlate_density(particle_traj, noise_traj, noise_file)
+        #### Output field properties to file in same directory as input file ####        
+        outfile = '/'.join((particle_file.split('/'))[:-1]) + '/%s_noise_correlation.npz' % quantity
+        np.savez(outfile, one_point_corr=corr, one_point_corr_sparse=corr_sparse, mean_density=quantity_mean, mean_density_sparse=quantity_mean_sparse, mean_noise=noise_mean)
     elif quantity=='pressure':
-        corr = correlate_pressure(particle_traj, noise_traj)
+        corr, corr_abs, noise_mean, quantity_mean = correlate_pressure(particle_traj)
+        #### Output field properties to file in same directory as input file ####        
+        outfile = '/'.join((particle_file.split('/'))[:-1]) + '/%s_noise_correlation.npz' % quantity
+        np.savez(outfile, one_point_corr=corr, one_point_corr_abs=corr_abs, mean_pressure=quantity_mean, mean_noise=noise_mean)
     else:
         print('Error: quantity not yet supported.')
         exit()
 
-    #### Output field properties to file in same directory as input file ####        
-    outfile = '/'.join((particle_file.split('/'))[:-1]) + '/%s_noise_correlation.npz' % property
-    np.savez(outfile, one_point_corr=corr)
+    
 
 def correlate_density(particle_traj, noise_traj, noise_file):
 
+    do_print_density=0
     #Compute scaled positions
     edges = particle_traj['edges']
     spacing = noise_traj['spacing']
@@ -43,11 +46,17 @@ def correlate_density(particle_traj, noise_traj, noise_file):
     nskip = int(nframes*0.4)
 
     #Get magnitude of noise field
-    noise_field = noise_traj['noise'][nskip:,:,:,:]
+    if noise_traj['noise'].shape[0]==1:
+        noise_field = np.repeat(noise_traj['noise'], repeats=nframes-nskip, axis=0)
+    else:
+        noise_field = noise_traj['noise'][nskip:,:,:,:]
+    print('noise field', noise_field.shape)
     mag_field = np.sqrt(noise_field[:,:,:,0].transpose(0,2,1)**2+noise_field[:,:,:,1].transpose(0,2,1)**2)
+    print('mag field', mag_field.shape)
 
     #Get density field
     density_field = get_density_field(nframes, nskip, particle_traj['pos'], edges, mag_field, spacing, dims)
+    density_field_sparse = get_density_field(nframes, nskip, particle_traj['pos'], edges, mag_field, spacing, dims, do_sparse=1)
     if do_print_density==1:
         np.savez('/'.join((noise_file.split('/'))[:-1]) + '/density_traj.npz', density=density_field)
 
@@ -55,17 +64,45 @@ def correlate_density(particle_traj, noise_traj, noise_file):
     noise_mean = np.average(mag_field)
     print(noise_mean)
     density_mean = np.average(density_field)
+    density_mean_sparse = np.average(density_field_sparse)
     print(density_mean)
     corr = np.average(np.multiply(mag_field, density_field)) - noise_mean*density_mean
+    corr_sparse = np.average(np.multiply(mag_field, density_field_sparse)) - noise_mean*density_mean_sparse
     print(corr)
+    print(corr_sparse)
 
-    return corr
+    return corr, corr_sparse, noise_mean, density_mean, density_mean_sparse
 
-def correlate_pressure(particle_traj, noise_traj):
-    return 0
+def correlate_pressure(particle_traj):
+
+    #TODO: make this work for 3d
+
+    nframes = particle_traj['pos'].shape[0]
+    nskip = int(nframes*0.4)
+
+    virial = particle_traj['virial'][nskip:,...]
+    pressure = -(virial[:,:,0]+virial[:,:,3])/2.0
+    active_force = particle_traj['active_force'][nskip:,...]
+    active_mag = np.sqrt(active_force[...,0]**2+active_force[...,1]**2)
+    print(pressure.shape)
+    print(active_mag.shape)
+
+    noise_mean = np.average(active_mag)
+    pressure_mean = np.average(pressure)
+    pressure_abs_mean = np.average(np.abs(pressure))
+
+    corr = np.average(np.multiply(active_mag, pressure)) - noise_mean*pressure_mean
+    corr_abs = np.average(np.multiply(active_mag, np.abs(pressure))) - noise_mean*pressure_abs_mean
+
+    print(corr)
+    print(corr_abs)
+    print(noise_mean)
+    print(pressure_mean)
+
+    return corr, corr_abs, noise_mean, pressure_mean
 
 @numba.jit(nopython=True)
-def get_density_field(nframes, nskip, pos, edges, mag_field, spacing, dims):
+def get_density_field(nframes, nskip, pos, edges, mag_field, spacing, dims, do_sparse=0):
 
     halfdiam = 0.5#*2**(1.0/6.0) #half the hard sphere diameter
     print(nframes-nskip)
@@ -80,39 +117,40 @@ def get_density_field(nframes, nskip, pos, edges, mag_field, spacing, dims):
     for t in range(nframes-nskip):
         for i in range(pos.shape[1]):
             density_field[t,scaled_pos_x_int[t,i],scaled_pos_y_int[t,i]] = 1.0
-            #Fill in adjacent sites that lie within the hard sphere diameter
-            #scan over a small area adjacent to the central site
-            xminind=(scaled_pos_x_int[t,i]-5+dims[0]) % dims[0]
-            xmaxind=(scaled_pos_x_int[t,i]+5) % dims[0]
-            yminind=(scaled_pos_y_int[t,i]-5+dims[1]) % dims[1]
-            ymaxind=(scaled_pos_y_int[t,i]+5) % dims[1]
-            for mx in range(scaled_pos_x_int[t,i]-5, scaled_pos_x_int[t,i]+5+1):
-                for my in range(scaled_pos_y_int[t,i]-5, scaled_pos_y_int[t,i]+5+1):
-                    xind = mx
-                    yind = my
-                    if xind>=dims[0]:
-                        xind -= dims[0]
-                    if xind<0:
-                        xind += dims[0]
-                    if yind>=dims[1]:
-                        yind -= dims[1]
-                    if yind<0:
-                        yind += dims[1]
-                    x = xind*spacing[0]+spacing[0]/2.0-edges[0]/2.0
-                    y = yind*spacing[1]+spacing[1]/2.0-edges[1]/2.0
-                    dx = x-pos[t,i,0]
-                    dy = y-pos[t,i,1]
-                    if dx>edges[0]/2.0:
-                        dx -= edges[0]
-                    if dx<=-edges[0]/2.0:
-                        dx += edges[0]
-                    if dy>edges[1]/2.0:
-                        dy -= edges[1]
-                    if dy<=-edges[1]/2.0:
-                        dy += edges[1]
-                    dist = np.sqrt(dx**2+dy**2)
-                    if dist<halfdiam:
-                        density_field[t,xind,yind] = 1.0
+            if do_sparse==0:
+                #Fill in adjacent sites that lie within the hard sphere diameter
+                #scan over a small area adjacent to the central site
+                xminind=(scaled_pos_x_int[t,i]-5+dims[0]) % dims[0]
+                xmaxind=(scaled_pos_x_int[t,i]+5) % dims[0]
+                yminind=(scaled_pos_y_int[t,i]-5+dims[1]) % dims[1]
+                ymaxind=(scaled_pos_y_int[t,i]+5) % dims[1]
+                for mx in range(scaled_pos_x_int[t,i]-5, scaled_pos_x_int[t,i]+5+1):
+                    for my in range(scaled_pos_y_int[t,i]-5, scaled_pos_y_int[t,i]+5+1):
+                        xind = mx
+                        yind = my
+                        if xind>=dims[0]:
+                            xind -= dims[0]
+                        if xind<0:
+                            xind += dims[0]
+                        if yind>=dims[1]:
+                            yind -= dims[1]
+                        if yind<0:
+                            yind += dims[1]
+                        x = xind*spacing[0]+spacing[0]/2.0-edges[0]/2.0
+                        y = yind*spacing[1]+spacing[1]/2.0-edges[1]/2.0
+                        dx = x-pos[t,i,0]
+                        dy = y-pos[t,i,1]
+                        if dx>edges[0]/2.0:
+                            dx -= edges[0]
+                        if dx<=-edges[0]/2.0:
+                            dx += edges[0]
+                        if dy>edges[1]/2.0:
+                            dy -= edges[1]
+                        if dy<=-edges[1]/2.0:
+                            dy += edges[1]
+                        dist = np.sqrt(dx**2+dy**2)
+                        if dist<halfdiam:
+                            density_field[t,xind,yind] = 1.0
 
     return density_field
 
